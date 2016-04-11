4628f0a351d2da786c5250a29af507d814571e2b
Li Chao <leetschau@gmail.com>
2016-04-11 12:21:03 +0800

    add function: optional sync to qiniu cloud

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Introduction
 
-OSS资源的备份、恢复工具
+OSS资源的备份、恢复工具。
+
+如果没有给备份程序ali-bak.py提供七牛认证参数(access-key & access-secret)，
+则不将备份压缩包同步到七牛上。
--- ali-bak.py
+++ ali-bak.py
@@ -4,23 +4,23 @@ import oss2
 import argparse
 import time
 import os
-from contextlib import contextmanager
 
 parser = argparse.ArgumentParser(
-    description='Backup an Aliyun OSS bucket on local machine')
+    description='Backup an Aliyun OSS bucket to local machine and qiniu')
 parser.add_argument('bucket', help='the bucket to be backuped')
 parser.add_argument('dst', help='the local backup folder')
-parser.add_argument('key',
-        help='the access key id of the bucket to be backuped')
-parser.add_argument('secret',
-        help='the access key secret of the bucket to be backuped')
+parser.add_argument('aliKey', help='the access key id of ali OSS')
+parser.add_argument('aliSecret', help='the access key secret of ali OSS')
+parser.add_argument('--qi-key', '-k', help='the access key of QiNiu')
+parser.add_argument('--qi-secret', '-s', help='the secret key of QiNiu')
 args = parser.parse_args()
 
-basedir = os.path.join(args.dst,
-              'ali%s-%s' % (time.strftime("%m.%d"), args.bucket))
+feature_name = 'ali%s-%s' % (time.strftime("%m.%d"), args.bucket)
+basedir = os.path.join(args.dst, feature_name)
 endpoint = 'http://oss-cn-beijing.aliyuncs.com'
-access_key_id, access_key_secret = (args.key, args.secret)
+qiniuBucket = 'oss-backup'
 
+from contextlib import contextmanager
 @contextmanager
 def cd(newdir):
     prevdir = os.getcwd()
@@ -31,20 +31,40 @@ def cd(newdir):
         os.chdir(prevdir)
 
 
-def save_file(bkt, rootdir, key):
-    curdir = os.path.join(rootdir, os.path.dirname(key))
-    filename = os.path.basename(key)
+def save_file(bkt, rootdir, obj):
+    curdir = os.path.join(rootdir, os.path.dirname(obj))
+    filename = os.path.basename(obj)
     print('save %s to %s' % (filename, curdir))
     if not os.path.exists(curdir): os.makedirs(curdir)
     with cd(curdir):
-        bkt.get_object_to_file(key, filename)
+        bkt.get_object_to_file(obj, filename)
 
+
+def sync2qiniu(filename, bkt, key, secret):
+    import qiniu
+    q = qiniu.Auth(key, secret)
+    token = q.upload_token(bkt, filename, 3600)
+    print('Send %s to qiniu ...' % filename)
+    ret, info = qiniu.put_file(token, filename, filename)
+    if (ret['key'] == filename) and (ret['hash'] == qiniu.etag(filename)):
+        print('upload successful')
 
 if __name__ == "__main__":
-    auth = oss2.Auth(access_key_id, access_key_secret)
+    auth = oss2.Auth(args.aliKey, args.aliSecret)
     service = oss2.Service(auth, endpoint)
     bucket = oss2.Bucket(auth, endpoint, args.bucket)
 
     for object_info in oss2.ObjectIterator(bucket):
         save_file(bucket, basedir, object_info.key)
 
+    archFile = feature_name + '.tar.gz'
+    import subprocess
+    subprocess.call('tar zcf %s %s' % (archFile, feature_name),
+            shell=True, cwd=args.dst)
+
+    if (args.qi_key is None) or (args.qi_secret is None):
+        print('backup successful')
+        import sys
+        sys.exit(0)
+    with cd(args.dst):
+        sync2qiniu(archFile, qiniuBucket, args.qi_key, args.qi_secret)
